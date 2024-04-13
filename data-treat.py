ce441fa27103fd8ef15381a601abbef4315b38ec
Matthieu Basset <bassetmatt@gmail.com>
2024-04-13 19:37:28 -0400

    Clean data-treat and update plots notebook

--- data-treat.py
+++ data-treat.py
@@ -10,8 +10,6 @@ def get_files(path):
     return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 
-# print(f'Photos: {get_files(PHOTOS)}')
-# print(f'Sketches: {get_files(SKETCHES)}')
 RENAME = True
 if RENAME:
     for p in get_files(PHOTOS):
@@ -19,12 +17,11 @@ if RENAME:
         name = f
         # Women
         name = re.sub('f-', 'F-', name)
-        name = re.sub('f1', 'F1-', name)
+        name = re.sub('f1-', 'F1-', name)
         # Men
         name = re.sub('m-', 'M-', name)
         name = re.sub('m1-', 'M1-', name)
         os.rename(f, name)
-        # print(f'{p:13s} -> {name}')
 
     for s in get_files(SKETCHES):
         f = os.path.join(SKETCHES, s)
@@ -37,10 +34,3 @@ if RENAME:
         name = re.sub('M2-', 'M-', name)
         name = re.sub('m1-', 'M1-', name)
         os.rename(f, name)
-        # print(f'{s:13s} -> {name}')
-
-dir = os.listdir(PHOTOS)
-print(f"{dir}")
-dir.sort()
-print(f"{dir}")
-# print(f"{os.listdir(PHOTOS)}")
